dbf5f190bf65b185b954882cab19136048999223
Marques960 <rafaeltavaresmarques06@gmail.com>
2023-12-28 03:49:42 +0000

    28/12

--- editdata.php
+++ editdata.php
@@ -1,11 +1,27 @@
-<?php 
-    include 'conexion.php';
+<?php
+include 'conexion.php';
 
-    $id = $_POST['id'];
-    $username = $_POST['username'];
-    $password = $_POST['password'];
-    $nivel = $_POST['nivel'];
+// Assuming POST method 
+$id = isset($_POST['id']) ? $_POST['id'] : '';
+$username = isset($_POST['username']) ? $_POST['username'] : '';
+$password = isset($_POST['password']) ? $_POST['password'] : '';
+$nivel = isset($_POST['nivel']) ? $_POST['nivel'] : '';
 
-    $connect-> query("UPDATE usuarios SET username='".$username."', password='".$password."', nivel = '".$nivel."' WHERE id=".$id);
+// Using prepared statements to prevent SQL injection
+$stmt = $connect->prepare("UPDATE usuarios SET username=?, password=?, nivel=? WHERE id=?");
+$stmt->bind_param("sssi", $username, $password, $nivel, $id);
 
+// Execute the prepared statement
+$result = $stmt->execute();
+
+if ($result) {
+    // Success
+    echo "Data updated successfully";
+} else {
+    // Error
+    echo "Error updating data: " . $stmt->error;
+}
+
+$stmt->close();
+$connect->close();
 ?>
